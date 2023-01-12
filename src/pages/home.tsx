466fb63efda25164eda5fc10b14ec1ea06b28a3b
jaqwe2301 <jaqwe2301@naver.com>
2023-01-13 01:40:08 +0900

    fix 콘텐츠 페이지 별 행간 수정 및 교육 현황 숫자 재수정

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -116,9 +116,9 @@ export const HomePage = () => {
   const main7TopAnimation = useScrollFadeIn(0.7, "100%", "0s");
   const main7BottomAnimation = useScrollFadeIn(0.1, "25%", "0s");
 
-  const main5Count1Animation = useScrollCount(3187, 2900, 1500, "명");
-  const main5Count2Animation = useScrollCount(2139, 1900, 1500, "개");
-  const main5Count3Animation = useScrollCount(274, 250, 1500, "시간");
+  const main5Count1Animation = useScrollCount(4320, 3800, 1500, "명");
+  const main5Count2Animation = useScrollCount(3312, 3000, 1500, "개");
+  const main5Count3Animation = useScrollCount(488, 463, 1500, "시간");
   const main5Count4Animation = useScrollCount(27, 10, 1500, "개교");
 
   return (
