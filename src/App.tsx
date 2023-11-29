1c3cd0fa8651c88c6b4a1cee71b8ed214c14bd84
jaqwe2301 <jaqwe2301@naver.com>
2023-11-29 17:45:21 +0900

    Feat: 페이지 렌더링 시 타 사이트로 이동

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,15 @@ import { useReactiveVar } from "@apollo/client";
 import { LoggedInRouter } from "./routers/logged-in-router";
 import { LoggedOutRouter } from "./routers/logged-out-router";
 import { isLoggedInVar } from "./apollo";
+import { useEffect } from "react";
 
 export const App = () => {
-  const isLoggedIn = useReactiveVar(isLoggedInVar);
-  return isLoggedIn ? <LoggedInRouter /> : <LoggedOutRouter />;
+  useEffect(() => {
+    window.location.href = "https://doroedu.oopy.io";
+  }, []);
+  return <></>;
+  // const isLoggedIn = useReactiveVar(isLoggedInVar);
+  // return isLoggedIn ? <LoggedInRouter /> : <LoggedOutRouter />;
 };
 
 export default App;
